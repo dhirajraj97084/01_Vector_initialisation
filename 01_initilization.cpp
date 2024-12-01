bb00e9120ce8ee44ab12e451721ec46bafa5d208
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-12-01 11:43:04 +0530

    first commit

---
+++ 01_initilization.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector> //vector header file
+using namespace std;
+
+int main(){
+    vector<int>vec={2,3,4,5}; //vector initialized size 4
+    cout<<vec[0]; //access vactor index 0
+    return 0;
+}
